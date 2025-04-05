41706f81bdeb08a9f08446c814d64a0cf0a7b698
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-04-04 18:38:55 -0600

    rutas api
    cambios nuevos de las rutas api

--- routes/api.php
+++ routes/api.php
@@ -1,39 +1,18 @@
 <?php
 
-use App\Http\Controllers\Api\CreatePermissionRolController;
-use App\Http\Controllers\Api\CreatePrimissionRolController;
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\AuthController;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+// Rutas protegidas por el middleware 'auth:sanctum'
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::prefix('auth')->group(function (){
-    Route::post('login', [AuthController::class,'login']);
+Route::middleware(['auth:sanctum','role:admin'])->group(function () {
+    Route::resource('roles', RoleController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'userProfile']);
+
+    //Agregar las demas rutas ejemplo pedido.
     
-    Route::post('refresh-token', [AuthController::class,'refresh']);
-    Route::post('register',[AuthController::class,'register']);
 });
-
-Route::middleware('auth:api')->prefix('users')->group(function (){
-    Route::get('/role',[CreatePermissionRolController::class, 'getRole'])->middleware('rol:Super Admin');
-    Route::post('/permissions',[CreatePermissionRolController::class,'createPermissionsAction'])->middleware('rol:Super Admin,Admin');
-    Route::post('/role',[CreatePermissionRolController::class,'store'])->middleware('rol:Super Admin');
-   
-});
-
-
-Route::middleware('auth:api')->group(function () {
-    Route::get('/admin-dashboard', function () {
-        return response()->json(['message' => 'Welcome to the admin dashboard']);
-    })->middleware('rol:Admin,Super Admin');
-});
-
-
-Route::middleware('auth:api')->prefix('users')->group(function () {
-    Route::post('logout', [AuthController::class,'logout']);
-});
-

ce2bdffcc5e9991ddddae0dc7902e91e3f6abc8c
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-05-31 15:29:12 -0600

    nuevos cambios del api

--- routes/api.php
+++ routes/api.php
@@ -37,5 +37,8 @@ Route::prefix('cliente')->middleware(['auth:sanctum', 'role:cliente'])->group(fu
     Route::get('/pedidos',[PedidosController::class, 'index']);
     Route::post('/pedidos',[PedidosController::class, 'storeCliente']);
     Route::get('/pagos', [PagoController::class, 'index']);
+    Route::post('/pagos', [PagoController::class, 'store']);
+    Route::post('/pagos/update/{id}', [PagoController::class, 'update']);
+    Route::post('/pagos/destroy/{id}', [PagoController::class, 'destroy']);
     Route::get('/metodos-pago', [MetodoPagoController::class, 'index']);
 });
